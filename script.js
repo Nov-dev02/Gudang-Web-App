a80d3932b107e6a07ca170980a93eb40c090e856
Nov <165930682+Nov-dev02@users.noreply.github.com>
2026-09-24 16:49:10 +0700

    add profile irgi

--- script.js
+++ script.js
@@ -9,7 +9,7 @@ const operatorsProfile = {
   "IRGI": { 
     id: "GDN-001", 
     name: "IRGI", 
-    photo: "" // Kosong = otomatis pakai inisial huruf "IR"
+    photo: "img/IRGI.jpg" // Kosong = otomatis pakai inisial huruf "IR"
   },
   "NOVAN": { 
     id: "GDN-002", 
